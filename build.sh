98aa1eb08599947f2a312f3cf51ddec528786d18
Sovcik, Jozef <jozef@sovcik.net>
2011-10-28 11:52:10 +0200

    small housekeeping

--- build.sh
+++ build.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
-echo "Copying files from source to build folder..."
-cp source/* build/
 
 echo "Calculating MD5 hash..."
 mfiles=`ls -1 source/*`
 for file in $mfiles; do
   md5h=`md5sum $file`
   echo "  > ${file##*/}"
-  sed -i -e "/${file##*/}/s|=>.*$|=> \'${md5h:0:32}\',|" build/ActivitiesReports_files.md5 
+  sed -i -e "/${file##*/}/s|=>.*$|=> \'${md5h:0:32}\',|" source/ActivitiesReports_files.md5 
 done
+
+echo "Copying files from source to build folder..."
+cp source/* build/
 
 echo "Creating package..."
 pkg="SugarCRM_6.x.x_Activities-Reports.zip"
 pushd build
-zip ../$pkg *
+zip -m $pkg *
 popd
-
-echo "Cleaning up...."
-rm -R build/*
 
 echo "Package $pkg created."
 
